524772e070738980d5ef7050acbf7908c4df5501
Zhong Ruoyu <zhongruoyu@outlook.com>
2022-05-04 11:25:01 +0800

    Add support for figures

--- assets/js/bootstrap.js
+++ assets/js/bootstrap.js
@@ -1,6 +1,18 @@
 "use strict";
 
 (function () {
+    for (const figure of document.getElementsByTagName("figure")) {
+        if (figure.classList.length === 0) {
+            figure.classList.add("figure");
+            for (const img of figure.getElementsByTagName("img")) {
+                img.classList.add("figure-img");
+            }
+            for (const figcaption of figure.getElementsByTagName("figcaption")) {
+                figcaption.classList.add("figure-caption");
+            }
+        }
+    }
+
     for (const table of document.getElementsByTagName("table")) {
         if (table.classList.length === 0) {
             table.classList.add("table");
