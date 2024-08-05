d1c43e011c4752d433de5f4926e6cc95d7114722
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-08-05 15:24:11 +0800

    Reduce whitespaces

--- assets/js/minimal-bootstrap.js
+++ assets/js/minimal-bootstrap.js
@@ -1,27 +1,27 @@
 "use strict";
 
 (function () {
-    const mains = document.getElementsByTagName("main");
-    if (mains.length !== 1) {
-        return;
-    }
-    const main = mains.item(0);
+  const mains = document.getElementsByTagName("main");
+  if (mains.length !== 1) {
+    return;
+  }
+  const main = mains.item(0);
 
-    for (const figure of main.getElementsByTagName("figure")) {
-        if (figure.classList.length === 0) {
-            figure.classList.add("figure");
-            for (const img of figure.getElementsByTagName("img")) {
-                img.classList.add("figure-img");
-            }
-            for (const figcaption of figure.getElementsByTagName("figcaption")) {
-                figcaption.classList.add("figure-caption");
-            }
-        }
+  for (const figure of main.getElementsByTagName("figure")) {
+    if (figure.classList.length === 0) {
+      figure.classList.add("figure");
+      for (const img of figure.getElementsByTagName("img")) {
+        img.classList.add("figure-img");
+      }
+      for (const figcaption of figure.getElementsByTagName("figcaption")) {
+        figcaption.classList.add("figure-caption");
+      }
     }
+  }
 
-    for (const table of main.getElementsByTagName("table")) {
-        if (table.classList.length === 0) {
-            table.classList.add("table");
-        }
+  for (const table of main.getElementsByTagName("table")) {
+    if (table.classList.length === 0) {
+      table.classList.add("table");
     }
+  }
 })();
